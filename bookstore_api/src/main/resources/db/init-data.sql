101a3eb0141a2075bf3985f4b3a177ad0638d085
SandraFre <sandra.email@gmail.com>
2021-12-28 22:02:21 +0200

    initial user and roles data added

--- bookstore_api/src/main/resources/db/init-data.sql
+++ bookstore_api/src/main/resources/db/init-data.sql
@@ -13,6 +13,19 @@ VALUES
 ('58a95af7-8b83-4a08-8001-0f865db8ea26', 'Tesė iš dErbervilių giminės', 'Thomas Hardy', 'Drama', '2007', 40, 15.99),
 ('abdee4f9-5763-4afc-85ed-98b2fdefb35f', 'Kvepalai. Vieno žudiko istorija', 'Patrick Suskind', 'Detektyvas', '2015', 110, 11.89);
 
+INSERT INTO USERS(id, username, name, surname, email, password)
+VALUES ('c2aa5f20-2441-40f8-8cce-d31dbd17bc84', 'user', 'Serbentautas', 'Bordiuras', 'user@bookstore.lt',
+        '{bcrypt}$2a$10$jYIbAef1H7S.womsk7MRtOCSEx/DgM7CZ1nNeLLzoZ/OPs0a25DV2'), /*pass->user*/
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', 'admin', 'Violeta', 'Rugiagele', 'admin@bookstore.lt',
+        '{bcrypt}$2a$10$VylYhXDaKC7W28tQTvYYkOdZIj2pnPVIobXOConbXy3xeBcF6Xuni'); /*pass->admin*/
 
+INSERT INTO ROLES(id, name)
+VALUES ('60dbb7bb-99a0-42eb-a837-8be6b697c074', 'USER'),
+       ('3906c549-44bf-494b-9537-5e1658a029a8', 'ADMIN');
+
+INSERT INTO USERS_ROLES (user_id, roles_id)
+VALUES ('c2aa5f20-2441-40f8-8cce-d31dbd17bc84', '60dbb7bb-99a0-42eb-a837-8be6b697c074'),
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', '60dbb7bb-99a0-42eb-a837-8be6b697c074'),
+       ('bd8968db-ad2a-4dd0-a0ab-7eebcc05427e', '3906c549-44bf-494b-9537-5e1658a029a8');
 
 
