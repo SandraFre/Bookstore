3ed3902c9b1c31e792ab4825a055ef4c3792e089
SandraFre <sandra.email@gmail.com>
2021-12-28 21:51:18 +0200

    user service created

--- bookstore_api/src/main/java/com/example/bookstore_api/repository/UserRepository.java
+++ bookstore_api/src/main/java/com/example/bookstore_api/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.bookstore_api.repository;
 
 import com.example.bookstore_api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@RequestMapping
 public interface UserRepository extends JpaRepository<User, UUID> {
 
     Optional<User> findByUsername(String username);
