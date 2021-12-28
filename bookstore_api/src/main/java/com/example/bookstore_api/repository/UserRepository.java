a8284570715146438a266833d95662e9f343d8bb
SandraFre <sandra.email@gmail.com>
2021-12-28 21:48:30 +0200

    user repository created

---
+++ bookstore_api/src/main/java/com/example/bookstore_api/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.bookstore_api.repository;
+
+import com.example.bookstore_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+}
