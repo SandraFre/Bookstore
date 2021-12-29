2a8b5f08c644b34f4718ecd58a5f9428ab2a7fa1
SandraFre <sandra.email@gmail.com>
2021-12-29 12:33:08 +0200

    store const created

--- bookstore_ui/src/store/storeConst.js
+++ bookstore_ui/src/store/storeConst.js
@@ -1,8 +1,4 @@
-// import {configureStore} from "@reduxjs/toolkit";
-//
-// const buildStore = ()=>{
-//     const store = configureStore({
-//         reducer: {''},
-//
-//     })
-// }
+const ADD_TO_CART = "ADD_TO_CART";
+const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+export { ADD_TO_CART, REMOVE_FROM_CART }
