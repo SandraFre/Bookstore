43d784b7eeccf604abad1e5b78d2ac948e1d5946
SandraFre <sandra.email@gmail.com>
2021-12-29 15:37:52 +0200

    provider added to App.js

--- bookstore_ui/src/App.js
+++ bookstore_ui/src/App.js
@@ -2,9 +2,12 @@ import Header from "./components/header/Header";
 import {BrowserRouter} from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Content from "./components/content/Content";
+import {Provider} from "react-redux";
+import store from "./store/store";
 
 function App() {
     return (
+        <Provider store={store}>
             <BrowserRouter>
                 <div className="container">
                     <Header/>
@@ -12,6 +15,7 @@ function App() {
                     <Footer/>
                 </div>
             </BrowserRouter>
+        </Provider>
     );
 }
 
