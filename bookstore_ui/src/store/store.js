12c7d28ea6d71eb3bd36ded66dd85ea99a3e9639
SandraFre <sandra.email@gmail.com>
2021-12-29 14:40:06 +0200

    user slice created and added to store

--- bookstore_ui/src/store/store.js
+++ bookstore_ui/src/store/store.js
@@ -1,11 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {logger} from "redux-logger/src";
 import cart, {loadCartFromLocalStorage, subscribeToStore} from "./slice/cartSlice";
+import user from "./slice/userSlice";
 
 const buildStore = () => {
     const store = configureStore({
         reducer: {
-            cart
+            cart, user
         },
         middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
         preloadedState: {
